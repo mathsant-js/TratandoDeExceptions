f149f5946f61ae2ec6bed86164c68acbc0f37b60
mathsant-js <matheus.santana18@fatec.sp.gov.br>
2026-02-27 19:40:27 -0300

    first commit - Aula 27/02/2026

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // O programa irá tentar executar os seguintes operações
+            int resultado = dividirPorZero(10, 0);
+            System.out.println("Resultado: " + resultado);
+
+            // Ele irá cair no catch, porque não é possível dividir 0
+        } catch (ArithmeticException e) {
+
+            // Ele irá exibir a mensagem abaixo
+            System.out.println("Não é possível dividir por 0 " + e);
+        }
+    }
+
+    public static int dividirPorZero(int numerador, int denominador) {
+        return numerador / denominador;
+    }
+}
